c6a9be78b2b7b0ce4f47f91513d09c443352d44a
rlagywnd4 <rlagywnd4@gmail.com>
2024-12-20 22:17:59 +0900

    feat: setup project

---
+++ src/main/java/com/sparta/team7instagram/Team7InstagramApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.team7instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team7InstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team7InstagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/team7instagram/Team7InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.team7instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team7InstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
