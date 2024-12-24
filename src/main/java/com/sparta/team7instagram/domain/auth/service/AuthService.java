87c7759e5f97651ce5e9e904a70453643b305e99
rlagywnd4 <rlagywnd4@gmail.com>
2024-12-24 14:14:52 +0900

    refactor: 회원가입, 로그인 위치 변경, 메소드 정리

--- src/main/java/com/sparta/team7instagram/domain/auth/service/AuthService.java
+++ src/main/java/com/sparta/team7instagram/domain/auth/service/AuthService.java
@@ -1,11 +1,49 @@
 package com.sparta.team7instagram.domain.auth.service;
 
+import com.sparta.team7instagram.domain.auth.config.PasswordEncoder;
+import com.sparta.team7instagram.domain.auth.dto.LoginUserRequestDto;
+import com.sparta.team7instagram.domain.auth.dto.SignupUserRequestDto;
+import com.sparta.team7instagram.domain.user.entity.UserEntity;
+import com.sparta.team7instagram.domain.user.repository.UserRepository;
+import com.sparta.team7instagram.global.util.SessionUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+@RequiredArgsConstructor
 @Service
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Long saveUser(
+            SignupUserRequestDto requestDto
+    ) {
+        checkEmailDuplicateAndThrow(requestDto.email());
+
+        String encodedPassword = passwordEncoder.encode(requestDto.password());
+
+        UserEntity userEntity = new UserEntity(requestDto.email(),encodedPassword, requestDto.name());
+        UserEntity createdUser = userRepository.save(userEntity);
+
+        return createdUser.getId();
+    }
+
+    public void login(
+            LoginUserRequestDto requestDto,
+            HttpServletRequest request
+    ) {
+        UserEntity findUserEntity = userRepository.findByEmail(requestDto.email())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Does not exist email = " + requestDto.email()));
+        if(!passwordEncoder.matches(requestDto.password(), findUserEntity.getPassword())){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "wrong password");
+        }
+
+        SessionUtil.createSession(findUserEntity.getId(), request);
+    }
 
     /**
      * id값으로 같은 유저인지 체크
@@ -21,5 +59,15 @@ public class AuthService {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
         }
         return true;
+    }
+
+    /**
+     * Email 중복 체크
+     * @param email 중복 체크할 email
+     */
+    public void checkEmailDuplicateAndThrow(String email){
+        if(userRepository.existsByEmail(email)){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"이미 존재하는 이메일입니다.");
+        }
     }
 }
--- src/main/java/com/sparta/team7instagram/domain/user/service/UserService.java
+++ src/main/java/com/sparta/team7instagram/domain/user/service/UserService.java
@@ -6,20 +6,14 @@ import com.sparta.team7instagram.domain.user.dto.response.UserResponseDto;
 import com.sparta.team7instagram.domain.user.entity.FollowEntity;
 import com.sparta.team7instagram.domain.user.entity.UserEntity;
 import com.sparta.team7instagram.domain.auth.config.PasswordEncoder;
-import com.sparta.team7instagram.domain.auth.dto.LoginUserRequestDto;
-import com.sparta.team7instagram.domain.auth.dto.SignupUserRequestDto;
 import com.sparta.team7instagram.domain.user.repository.FollowRepository;
 import com.sparta.team7instagram.domain.user.repository.UserRepository;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -29,34 +23,6 @@ public class UserService {
     private final UserRepository userRepository;
     private final FollowRepository followRepository;
     private final PasswordEncoder passwordEncoder;
-
-    public void saveUser(
-            SignupUserRequestDto requestDto
-    ) {
-        Optional<UserEntity> findUser = userRepository.findByEmail(requestDto.getEmail());
-
-        if(findUser.isPresent()){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"이미 존재하는 이메일입니다.");
-        }
-        String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
-        UserEntity userEntity = new UserEntity(requestDto.getEmail(),encodedPassword, requestDto.getName());
-
-        userRepository.save(userEntity);
-    }
-
-    public void login(
-            LoginUserRequestDto requestDto,
-            HttpServletRequest request
-    ) {
-        UserEntity findUserEntity = userRepository.findByEmail(requestDto.getEmail())
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Does not exist email = " + requestDto.getEmail()));
-        if(!passwordEncoder.matches(requestDto.getPassword(), findUserEntity.getPassword())){
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "wrong password");
-        }
-
-        HttpSession session = request.getSession(true);
-        session.setAttribute("userId", findUserEntity.getId());
-    }
 
     /**
      * 유저 프로필 조회
