59f3ee58d073f3c2a09353b8075b518d7f08dbde
SeuneKeun <eiziong1@gmail.com>
2024-12-24 10:47:40 +0900

    feat: BaseEntity 추가

--- src/main/java/com/sparta/team7instagram/Team7InstagramApplication.java
+++ src/main/java/com/sparta/team7instagram/Team7InstagramApplication.java
@@ -2,8 +2,10 @@ package com.sparta.team7instagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Team7InstagramApplication {
 
 	public static void main(String[] args) {
