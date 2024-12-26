75e8117732580d31e0c0112bd4db06b009b00357
SeuneKeun <eiziong1@gmail.com>
2024-12-26 12:08:00 +0900

    refactor: 필터 예외도 ExceptionHandler 와 똑같이 응답을 할 수 있도록 수정

--- src/main/java/com/sparta/team7instagram/domain/auth/filter/LoginFilter.java
+++ src/main/java/com/sparta/team7instagram/domain/auth/filter/LoginFilter.java
@@ -1,10 +1,15 @@
 package com.sparta.team7instagram.domain.auth.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.team7instagram.global.exception.UnauthorizedException;
+import com.sparta.team7instagram.global.exception.error.ErrorCode;
+import com.sparta.team7instagram.global.exception.error.ErrorResponse;
+import com.sparta.team7instagram.global.util.SessionUtil;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.util.PatternMatchUtils;
 
 import java.io.IOException;
@@ -23,28 +28,31 @@ public class LoginFilter implements Filter {
 
         // 다양한 기능을 사용하기 위해 다운 캐스팅
         HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
         String requestURI = httpRequest.getRequestURI();
 
         log.info("로그인 필터 로직 실행");
 
         // 로그인을 체크 해야하는 URL인지 검사
         // whiteListURL에 포함된 경우 true 반환 -> !true = false
-        if (!isWhiteList(requestURI)) {
-            HttpSession session = httpRequest.getSession(false);
+        try {
+            if (!isWhiteList(requestURI) && !HttpMethod.GET.matches(httpRequest.getMethod())) {
+                SessionUtil.validateSession(httpRequest.getSession(false));
+            }
+        } catch (UnauthorizedException e) {
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            httpResponse.setContentType("application/json");
+            httpResponse.setCharacterEncoding("UTF-8");
 
-                // 로그인하지 않은 사용자인 경우
-                if (session == null || session.getAttribute("USER-ID") == null) {
-                    HttpServletResponse httpResponse = (HttpServletResponse) response;
-                    httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                    httpResponse.setContentType("application/json;charset=UTF-8");
-                    httpResponse.getWriter().write("로그인이 필요합니다.");
-                    return;
-                }
+            ErrorResponse errorResponse = new ErrorResponse(ErrorCode.UNAUTHORIZED.getStatus(), ErrorCode.UNAUTHORIZED.getMessage());
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            httpResponse.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+            return;
         }
 
         chain.doFilter(request, response);
-
-
     }
 
     // 로그인 여부를 확인하는 URL인지 체크하는 메서드
