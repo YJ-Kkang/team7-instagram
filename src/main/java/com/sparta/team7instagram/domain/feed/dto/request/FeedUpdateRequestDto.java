a201f00cca0ca7772b5be4665802a45ac59dd5a4
SeuneKeun <eiziong1@gmail.com>
2024-12-26 12:06:15 +0900

    fix: 사용하지 않는 메소드, 레포지토리 제거
    - FeedLikeRepository
    - FeedUpdateRequestDto toEntity()

--- src/main/java/com/sparta/team7instagram/domain/feed/dto/request/FeedUpdateRequestDto.java
+++ src/main/java/com/sparta/team7instagram/domain/feed/dto/request/FeedUpdateRequestDto.java
@@ -1,19 +1,9 @@
 package com.sparta.team7instagram.domain.feed.dto.request;
 
-import com.sparta.team7instagram.domain.feed.entity.FeedEntity;
-import com.sparta.team7instagram.domain.user.entity.UserEntity;
-
 import java.util.Set;
 
 public record FeedUpdateRequestDto(
         String content,
         Set<String> tags
 ) {
-
-    public FeedEntity toEntity(UserEntity user) {
-        return FeedEntity.builder()
-                .content(this.content)
-                .user(user)
-                .build();
-    }
 }
--- src/main/java/com/sparta/team7instagram/domain/feed/repository/FeedLikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.team7instagram.domain.feed.repository;
-
-import com.sparta.team7instagram.domain.feed.entity.FeedLikeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedLikeRepository extends JpaRepository<FeedLikeEntity, Long> {
-}
