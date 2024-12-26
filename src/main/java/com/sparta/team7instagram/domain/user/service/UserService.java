68da81f03969d747ed4ec94798d1b6fb2619f15e
rlagywnd4 <rlagywnd4@gmail.com>
2024-12-26 12:04:06 +0900

    fix: 불필요한 코드 제거

--- src/main/java/com/sparta/team7instagram/domain/user/service/UserService.java
+++ src/main/java/com/sparta/team7instagram/domain/user/service/UserService.java
@@ -12,14 +12,12 @@ import com.sparta.team7instagram.domain.user.repository.FollowRepository;
 import com.sparta.team7instagram.domain.user.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserService {
